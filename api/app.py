47728c396540c05d6ceacd099a07c540a69c14d1
ramirez.alberto <ramirez.alberto@zoho.com>
2023-02-28 20:40:53 -0800

    Create and register a resource

--- api/app.py
+++ api/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+api = Api(app)
 
 class Publicacion(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -19,7 +21,21 @@ class Publicacion_Schema(ma.Schema):
 post_schema = Publicacion_Schema()
 posts_schema = Publicacion_Schema(many = True)
 
+class RecursoListarPublicaciones(Resource):
+    def get(self):
+        publicaciones = Publicacion.query.all()
+        return posts_schema.dump(publicaciones)
+    def post(self):
+        nueva_publicacion = Publicacion(
+            titulo = request.json['titulo'],
+            contenido = request.json['contenido']
+        )
 
+        db.session.add(nueva_publicacion)
+        db.session.commit()
+        return post_schema.dump(nueva_publicacion)
+    
+api.add_resource(RecursoListarPublicaciones, '/publicaciones')
 
 if __name__ == '__main__':
     app.run(debug=True)
